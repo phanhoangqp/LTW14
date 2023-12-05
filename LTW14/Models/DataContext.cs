bb4eef187bb6d34b43eaf3d42c44c11083762816
admin <admin@LAPTOP-VCM50FG8>
2023-12-05 16:34:01 +0700

    Add project files.

---
+++ LTW14/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LTW14.Models
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { 
+		}
+		public DbSet<Menu> Menus { get; set; }	
+	}
+
+}
